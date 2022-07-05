749161e5290739a6188d44d168c9bfbef3f03ccd
Rohit <rohitbe5e@gmail.com>
2022-07-05 11:31:42 +0530

    Basic logic is done!

--- Methods.py
+++ Methods.py
@@ -2,6 +2,35 @@ import random
 
 
 class Attack:
+    var = 0  # attribute, for example attribute of car is model, year made, engine power, etc
+
+    def __init__(self, name, the_level):  # method
+        """
+
+        :rtype: object
+        """
+        self.name = name  # name, level r attributes, init is initializing them for objects
+        self.level = the_level  # assigning parameters to arguments
+
+    # dragonslayer = Attack(dragonslayer, 100)
+    # print(dragonslayer.name)
+    # the object is dragonslayer & it's level is 100
+    # dragonslayer.get_defensive_roll()
+    # def printWeaponName(self):
+    # print("this is" + self.name) #when object is made self will be replaced with it
+
+    def __repr__(self):  # self refers to the object that is using this method, for car it is drive, stop, etc
+        return "Creature: {} of level {}".format(
+            self.name, self.level
+        )
+
+    def power(self):
+        return self.level
+
+
+
+class Monster:
+
     def __init__(self, name, the_level):
         """
 
@@ -15,11 +44,8 @@ class Attack:
             self.name, self.level
         )
 
-    def get_defensive_roll(self):
-        return random.randint(1, 12) * self.level
+    def power(self):
+        return self.level
 
 
-class Sword(Attack):
-    def get_defensive_roll(self):
-        base_roll = super().get_defensive_roll()
-        return base_roll / 2
+
---
+++ Program.py
@@ -0,0 +1,36 @@
+from Methods import *
+
+MonsterLevel = 0
+Monster_list = ["Drunk Skull", "Blue Knight", "Zodd"]
+MonsterStr = Monster_list[random.randint(0, 2)]
+print(MonsterStr + " has appeared")
+if MonsterStr == "Drunk Skull":
+    MonsterLevel = Monster("Drunk Skull", random.randint(50, 100)).power()
+elif MonsterStr == "Blue Knight":
+    MonsterLevel = Monster("Blue Knight", random.randint(50, 100)).power()
+elif MonsterStr == "Zodd":
+    MonsterLevel = Monster("Zodd", random.randint(80, 100)).power()
+
+AttackLevel = 0
+Attack_list = ["Sword", "Magic", "Arrow"]
+print("choose attack method Struggler!")
+attack_index = int(input("enter 0, 1 or 2"))
+
+try:
+    AttackStr = Attack_list[attack_index]
+except:
+    print("Failure")
+    exit()
+print(f"you have choosen {AttackStr}")
+if AttackStr == "Sword":
+    AttackLevel = Attack("Sword", random.randint(0, 100)).power()
+elif AttackStr == "Magic":
+    AttackLevel = Attack("Magic", random.randint(0, 100)).power()
+elif AttackStr == "Arrow":
+    AttackLevel = Attack("Arrow", random.randint(0, 100)).power()
+
+m = MonsterLevel - AttackLevel
+if m > 0:
+    print("Failure")
+else:
+    print("Success")
