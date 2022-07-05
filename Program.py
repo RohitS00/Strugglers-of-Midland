bafdbd5a49a4b60106e87a6979776faec588ba90
Rohit <rohitbe5e@gmail.com>
2022-07-05 12:05:26 +0530

    chances logic added

--- Program.py
+++ Program.py
@@ -14,23 +14,33 @@ elif MonsterStr == "Zodd":
 AttackLevel = 0
 Attack_list = ["Sword", "Magic", "Arrow"]
 print("choose attack method Struggler!")
+
+
+def fight(attack_index):
+    try:
+        AttackStr = Attack_list[attack_index]
+    except:
+        print("Failure")
+        exit()
+    print(f"you have choosen {AttackStr}")
+    if AttackStr == "Sword":
+        AttackLevel = Attack("Sword", random.randint(0, 100)).power()
+    elif AttackStr == "Magic":
+        AttackLevel = Attack("Magic", random.randint(0, 100)).power()
+    elif AttackStr == "Arrow":
+        AttackLevel = Attack("Arrow", random.randint(0, 100)).power()
+    return AttackLevel
+
+
 attack_index = int(input("enter 0, 1 or 2"))
-
-try:
-    AttackStr = Attack_list[attack_index]
-except:
-    print("Failure")
-    exit()
-print(f"you have choosen {AttackStr}")
-if AttackStr == "Sword":
-    AttackLevel = Attack("Sword", random.randint(0, 100)).power()
-elif AttackStr == "Magic":
-    AttackLevel = Attack("Magic", random.randint(0, 100)).power()
-elif AttackStr == "Arrow":
-    AttackLevel = Attack("Arrow", random.randint(0, 100)).power()
-
-m = MonsterLevel - AttackLevel
-if m > 0:
-    print("Failure")
-else:
-    print("Success")
+m = MonsterLevel - fight(attack_index)
+loopVar = 0
+while loopVar <= 5:
+    if m > 0:
+        print("Failure")
+        attack_index = int(input("enter 0, 1 or 2"))
+        m = MonsterLevel - fight(attack_index)
+        loopVar += 1
+    else:
+        print("Success")
+        exit()
